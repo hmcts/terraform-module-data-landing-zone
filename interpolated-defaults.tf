9191908e4707b794d26955c4c1f38d4aa0e309ea
Alex Bance <Alex.Bance@justice.gov.uk>
2023-10-09 17:19:40 +0100

    fix: simplify short name logic

--- interpolated-defaults.tf
+++ interpolated-defaults.tf
@@ -6,7 +6,7 @@ locals {
   is_sbox              = length(regexall(".*(s?box).*", var.env)) > 0
   admin_group          = local.is_prod ? "DTS Platform Operations SC" : "DTS Platform Operations"
   name                 = var.name != null ? var.name : "datalanding"
-  short_name           = "${substr(replace(local.name, "-", ""), 0, 3)}${substr(replace(local.name, "-", ""), -3, -1)}"
+  short_name           = substr(local.name, 0, 11)
   resource_group_names = ["network", "management", "logging", "runtimes", "storage", "external-storage", "metadata", "shared-integration", "shared-product", "di001", "di002", "dp001", "dp002"]
 
   storage_accounts = {
