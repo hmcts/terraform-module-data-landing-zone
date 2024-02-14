fd19f25c3a2e680b81080e0a4869a625e31ab604
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2024-02-14 15:48:19 +0000

    Removed az

--- metadata.tf
+++ metadata.tf
@@ -70,7 +70,6 @@ module "vm_database" {
   vm_size              = local.vm_size
   vm_admin_name        = "sql_${random_string.vm_username.result}"
   vm_admin_password    = random_password.vm_password.result
-  vm_availabilty_zones = ""
   os_disk_size_gb      = 127
   vm_resource_group    = azurerm_resource_group.this["metadata"].name
   vm_subnet_id         = module.networking.subnet_ids["vnet-services"]
