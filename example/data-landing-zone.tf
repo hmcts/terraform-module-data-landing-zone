f0187669dc7aeec9d25423542d96580e75a9f80f
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-07 16:10:48 +0100

    add required providers

--- example/data-landing-zone.tf
+++ example/data-landing-zone.tf
@@ -1,9 +1,5 @@
 module "data_landing_zone" {
   source          = "../."
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
 
   providers = {
     azurerm        = azurerm
--- example/provider.tf
+++ example/provider.tf
@@ -54,3 +54,30 @@ provider "databricks" {
   alias = "databricks"
   host  = "https://adb-1879076228317698.18.azuredatabricks.net"
 }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      version               = ">= 3.116.0"
+      configuration_aliases = [azurerm.hub, azurerm.ssptl, azurerm.cftptl, azurerm.soc, azurerm.cnp, azurerm.dcr]
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.43.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
+    databricks = {
+      source                = "databricks/databricks"
+      version               = ">= 1.69.0"
+      configuration_aliases = [databricks]
+    }
+  }
+}
