7fcbfcaa698539da7831da5d12de697ead053d58
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-07 11:49:06 +0100

    use env var

--- databricks.tf
+++ databricks.tf
@@ -2,6 +2,7 @@ module "databricks_cluster" {
   providers = {
     databricks = databricks
   }
+
   source                   = "./modules/databricks/Cluster"
   depends_on               = [module.shared_integration_databricks]
   databricks_workspace_url = module.shared_integration_databricks.workspace_url
