be8e5a26f5a72ce05fc7d1b5ae59fb94ddff56c6
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-04-07 10:42:48 +0100

    update providers and vars

--- modules/databricks/Cluster/variables.tf
+++ modules/databricks/Cluster/variables.tf
@@ -9,19 +9,3 @@ variable "initialization_complete" {
   type        = string
   default     = null
 }
-
-variable "tenant_id" {
-  type        = string
-  description = "The Azure tenant ID"
-}
-
-variable "client_id" {
-  type        = string
-  description = "The Azure client ID (service principal ID)"
-}
-
-variable "client_secret" {
-  type        = string
-  description = "The Azure client secret (service principal secret)"
-  sensitive   = true
-}
