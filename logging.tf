f3ea322b36b1ea774830c363b40a64c44216af4b
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-11 15:54:03 +0100

    DTSPO-34656: update key vault module branch

--- logging.tf
+++ logging.tf
@@ -1,5 +1,5 @@
 module "logging_vault" {
-  source              = "github.com/hmcts/cnp-module-key-vault?ref=DTSPO-31695/remove-jenkins-ptl-access"
+  source              = "github.com/hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
   name                = length("${local.name}-logging-${var.env}") > 24 ? "${local.short_name}-logging-${var.env}" : "${local.name}-logging-${var.env}"
   product             = "data-landing"
   env                 = var.env
